pop-os/system76-power

commit 1e1599a9dd1208820fcb2e374fea9f72b35e50e7

daemon: Always enable GPU power
Always enable GPU power and rely on RTD3 to put the device into D3.
Prevents the device from being removed from the bus on boot.

Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/daemon.rs
@@ -234,11 +234,11 @@ pub async fn daemon() -> Result<(), String> {
         None
     };
 
-    log::info!("Setting automatic graphics power");
-    match daemon.auto_graphics_power() {
+    log::info!("Setting graphics power");
+    match daemon.set_graphics_power(true) {
         Ok(()) => (),
         Err(err) => {
-            log::warn!("Failed to set automatic graphics power: {}", err);
+            log::warn!("Failed to set graphics power: {}", err);
         }
     }
 

Plexvola/system76-power-openrc

commit 77def87a713810146097ee5190a7c0863384e645

Disable dGPU if it doesn't support runtimepm
If a device does not support runtime power management, then remove it
from the bus when in integrated mode.

Fixes: 1e1599a9dd12 ("daemon: Always enable GPU power")
Signed-off-by: Tim Crawford <tcrawford@system76.com>

src/daemon.rs
@@ -234,11 +234,11 @@ pub async fn daemon() -> Result<(), String> {
         None
     };
 
-    log::info!("Setting graphics power");
-    match daemon.set_graphics_power(true) {
+    log::info!("Setting automatic graphics power");
+    match daemon.auto_graphics_power() {
         Ok(()) => (),
         Err(err) => {
-            log::warn!("Failed to set graphics power: {}", err);
+            log::warn!("Failed to set automatic graphics power: {}", err);
         }
     }
 

src/graphics.rs
@@ -504,8 +504,12 @@ impl Graphics {
     }
 
     pub fn auto_power(&self) -> Result<(), GraphicsDeviceError> {
+        // Only disable power if in integrated mode and the device does not
+        // support runtime power management.
         let vendor = self.get_vendor()?;
-        self.set_power(vendor != GraphicsMode::Integrated)
+        let power = vendor != GraphicsMode::Integrated || self.gpu_supports_runtimepm()?;
+
+        self.set_power(power)
     }
 
     fn switchable_or_fail(&self) -> Result<(), GraphicsDeviceError> {